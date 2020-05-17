eb70c487eb3276a4a7bb6983d4485e4f52d2b61a
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-05-17 17:08:31 +0530

    Version changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/client/index.js',
     module: {
         rules: [
@@ -18,6 +19,15 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html',
-        })
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
     ]
 }
